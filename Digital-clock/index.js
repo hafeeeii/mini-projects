9682722bfbf6562624c4a644d200a768e847a54c
iamhafis <121962161+hafis1017@users.noreply.github.com>
2023-07-05 13:34:52 +0530

    Add files via upload

---
+++ Digital-clock/index.js
@@ -0,0 +1,30 @@
+let hourEl = document.getElementById('hour');
+let minuteEl = document.getElementById('minute');
+let secondEl = document.getElementById('second');
+let ampmEl = document.getElementById('ampm');
+
+function updateClock(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = 'AM'
+
+    if(h>12){
+        h = h-12
+        ampm = 'PM'
+    }
+    
+    h = h<10 ? '0' + h : h;
+    m = m<10 ? '0' + m : m;
+    s = s<10 ? '0' + s : s;
+
+    hourEl.innerText = h
+    minuteEl.innerText = m
+    secondEl.innerText = s
+    ampmEl.innerText = ampm
+
+    setTimeout(()=>{
+        updateClock()
+    },1000)
+}
+updateClock()
